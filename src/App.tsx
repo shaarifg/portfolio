93b92ee7c8aa420b540f264cd84877301b1f93a7
shaarifg <ermohdsharif@gmail.com>
2023-12-05 22:52:53 +0530

    router set-up

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Navbar from './components/navbar/Navbar';
 
 // import Projects from './modules/projects/Projects';
 
+
+
 function App() {
   return (
     <div className="App bg-yellow-500">
--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,28 @@ import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from './routes/root';
+import ErrorPage from './pages/ErrroPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement:<ErrorPage/>
+  },
+]);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
   </React.StrictMode>
 );
