b9e84930005a8b275ff4b21e3d0e805ee2b34e83
shaarifg <ermohdsharif@gmail.com>
2023-12-07 00:06:27 +0530

    blogs data and interface

---
+++ src/components/blogs/blogs.data.ts
@@ -0,0 +1,20 @@
+import { BlogPost } from "./blogs.interface";
+
+export const BlogsData:BlogPost[] = [
+    {
+      id: 1,
+      title: 'Introduction to ReactJS',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+      author: 'John Doe',
+      date: '2023-12-06',
+      tags: ['React', 'JavaScript', 'Web Development'],
+    },
+    {
+      id: 2,
+      title: 'Styling in React with CSS Modules',
+      content: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...',
+      author: 'Jane Smith',
+      date: '2023-12-07',
+      tags: ['React', 'CSS Modules', 'Styling'],
+    },
+  ];
---
+++ src/components/blogs/blogs.interface.ts
@@ -0,0 +1,11 @@
+// BlogPost.ts
+export interface BlogPost {
+    id: number;
+    title: string;
+    content: string;
+    author: string;
+    date: string;
+    tags: string[];
+  }
+
+  
