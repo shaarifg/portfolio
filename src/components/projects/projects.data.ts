975a6248dc8d42489d9d4406492e930b48429a1c
shaarifg <ermohdsharif@gmail.com>
2023-12-06 23:32:01 +0530

    added data

--- src/components/projects/projects.data.ts
+++ src/components/projects/projects.data.ts
@@ -1,8 +1,49 @@
 import { Project } from "./projects.interface";
 
-export const Projects :Project[]=[{
-    id:1,
-    title:"string",
-    desc:"string",
-    imagesLink:"string"
-} ]
+export const Projects: Project[] = [
+  {
+    id: 1,
+    category:"personal",
+    title: "Telegraph",
+    tag: "A real-time content management web application (MERN)",
+    desc: "Implemented frontend with ReactJs and developed a user-friendly layout with integration of MaterialUI components.Created REST APIs using ExpressJs and for database used MongoDb.Developed three modules viz. Admin Module, Author Module, User Module and created their respective Dashboard.Implemented functionalities like Sign-Up, Sign-In and Forget Password with proper authentication & authorization.Integrated Rich Markdown Text Editor to create new post with preview image, title etc. Admin has control over all the features of application and can manage all Posts, all Users and User’s Profiles and onlyAdmin can change a user into an Author. Admin can perform CRUD operations on all features",
+    github_link:
+      "https://github.com/shaarifg/telegraph-a-content-management-system",
+    imagesLink:
+      "https://media.istockphoto.com/id/1356110094/vector/content-management-system-cms-banner.jpg?s=612x612&w=0&k=20&c=HEJzWfRS0MiqBsysxJcG6frJDATwcpboMIIR4ERB9XY=",
+  },
+  {
+    id: 2,
+    category:"personal",
+    title: "Telegraph",
+    tag: "A real-time content management web application (MERN)",
+    desc: "Implemented frontend with ReactJs and developed a user-friendly layout with integration of MaterialUI components.Created REST APIs using ExpressJs and for database used MongoDb.Developed three modules viz. Admin Module, Author Module, User Module and created their respective Dashboard.Implemented functionalities like Sign-Up, Sign-In and Forget Password with proper authentication & authorization.Integrated Rich Markdown Text Editor to create new post with preview image, title etc. Admin has control over all the features of application and can manage all Posts, all Users and User’s Profiles and onlyAdmin can change a user into an Author. Admin can perform CRUD operations on all features",
+    github_link:
+      "https://github.com/shaarifg/telegraph-a-content-management-system",
+    imagesLink:
+      "https://media.istockphoto.com/id/1356110094/vector/content-management-system-cms-banner.jpg?s=612x612&w=0&k=20&c=HEJzWfRS0MiqBsysxJcG6frJDATwcpboMIIR4ERB9XY=",
+  },
+  {
+    id: 3,
+    category:"personal",
+    title: "Telegraph",
+    tag: "A real-time content management web application (MERN)",
+    desc: "Implemented frontend with ReactJs and developed a user-friendly layout with integration of MaterialUI components.Created REST APIs using ExpressJs and for database used MongoDb.Developed three modules viz. Admin Module, Author Module, User Module and created their respective Dashboard.Implemented functionalities like Sign-Up, Sign-In and Forget Password with proper authentication & authorization.Integrated Rich Markdown Text Editor to create new post with preview image, title etc. Admin has control over all the features of application and can manage all Posts, all Users and User’s Profiles and onlyAdmin can change a user into an Author. Admin can perform CRUD operations on all features",
+    github_link:
+      "https://github.com/shaarifg/telegraph-a-content-management-system",
+    imagesLink:
+      "https://media.istockphoto.com/id/1356110094/vector/content-management-system-cms-banner.jpg?s=612x612&w=0&k=20&c=HEJzWfRS0MiqBsysxJcG6frJDATwcpboMIIR4ERB9XY=",
+  },
+  {
+    id: 4,
+    category:"professional",
+    title: "Telegraph",
+    tag: "A real-time content management web application (MERN)",
+    desc: "Implemented frontend with ReactJs and developed a user-friendly layout with integration of MaterialUI components.Created REST APIs using ExpressJs and for database used MongoDb.Developed three modules viz. Admin Module, Author Module, User Module and created their respective Dashboard.Implemented functionalities like Sign-Up, Sign-In and Forget Password with proper authentication & authorization.Integrated Rich Markdown Text Editor to create new post with preview image, title etc. Admin has control over all the features of application and can manage all Posts, all Users and User’s Profiles and onlyAdmin can change a user into an Author. Admin can perform CRUD operations on all features",
+    github_link:
+      "https://github.com/shaarifg/telegraph-a-content-management-system",
+    imagesLink:
+      "https://media.istockphoto.com/id/1356110094/vector/content-management-system-cms-banner.jpg?s=612x612&w=0&k=20&c=HEJzWfRS0MiqBsysxJcG6frJDATwcpboMIIR4ERB9XY=",
+  },
+  
+];
---
+++ src/components/skills/skills.data.ts
@@ -0,0 +1,9 @@
+import { Skills } from "./skills.interface";
+
+export const SkillsData: Skills[] = [
+  {
+    id:1,
+    name: "sharif",
+    icon: "shrif",
+  },
+];
