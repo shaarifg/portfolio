177b16644a2833a088b72fb96e993779b1ad47c4
shaarifg <ermohdsharif@gmail.com>
2023-12-07 23:12:43 +0530

    screens and theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '640px', // for mobile
+        md: '768px', //for tablet 
+        lg: '1024px', // for laptop
+        xl: '1280px', // big screens
+        // Add more screen sizes as needed
+      },
+      colors: {
+        theme1: {
+          primary: "#3498db",
+          secondary: "#2ecc71",
+        },
+        theme2: {
+          primary: "#e74c3c",
+          secondary: "#f39c12",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
